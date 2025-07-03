021b44b3871021dc2e14c089326e839adb6f161b
CoffeeeAtNight <98992091+CoffeeeAtNight@users.noreply.github.com>
2025-07-03 14:31:04 +0200

    Update BLK_LED.c

--- BLK_LED/main/BLK_LED.c
+++ BLK_LED/main/BLK_LED.c
@@ -48,14 +48,12 @@ void configure_gpio() {
   gpio_config_t io_conf;
   // Disable interrupts
   io_conf.intr_type = GPIO_INTR_DISABLE;
-  // Set as input mode
   io_conf.mode = GPIO_MODE_INPUT;
-  // Bit mask of the pin that you want to set
+  // Bit mask of the pin
   io_conf.pin_bit_mask = (1ULL << BTN_PIN);
   // Disable pull-up resistor
   io_conf.pull_up_en = 1;
   // Enable pull-down resistor
   io_conf.pull_down_en = 0;
-  // Configure GPIO with the given settings
-  gpio_config(&io_conf);
+  gpio_config(&io_conf); // Pass config of GPIO
 }
